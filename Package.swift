60c1721353be95ff51e548ab2624d50a45f457c4
Jake Waldon <jakewaldron@users.noreply.github.com>
2025-05-22 12:09:11 -0700

    Update Package.swift to force dynamic

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AdaptiveCards",
+            type: .dynamic,
             targets: ["AdaptiveCards", "ObjectModel"]),
     ],
     dependencies: [
